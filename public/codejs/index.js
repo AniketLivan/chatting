847f60e5404b3cdcd26612099663be9196835d9e
AniketLivan <kumaraniket383@gmail.com>
2019-08-04 15:41:45 +0530

    added form to show messages

--- public/codejs/index.js
+++ public/codejs/index.js
@@ -10,4 +10,20 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) =>{
     console.log('newMessage', message);
+
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li); 
+});
+
+jQuery('#message-form').on('submit', (e) => {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, () => {
+
+    });
 });
