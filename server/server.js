683d0adf48d6282a13a19903f04ae96b74679c9e
AniketLivan <kumaraniket383@gmail.com>
2019-08-04 18:13:43 +0530

    added UI for the page

--- server/server.js
+++ server/server.js
@@ -5,7 +5,10 @@ const socketIO = require('socket.io');
 const Filter = require('bad-words');
 
 const publicPath = path.join(__dirname, '../public');
-const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {
+    generateMessage,
+    generateLocationMessage
+} = require('./utils/message');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
@@ -16,23 +19,23 @@ io.on('connection', (socket) => {
     console.log('new user connected');
 
     socket.on('disconnect', () => {
-        console.log('User disconnected to server'); 
+        console.log('User disconnected to server');
     });
 
-    socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message, callback) => {
         const filter = new Filter();
 
-        if(filter.isProfane(message)){
+        if (filter.isProfane(message)) {
             return callback('Profanity is not allowed');
         }
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
-        
+        callback();
+
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
@@ -41,10 +44,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on('sendLocation', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude, coords.longitude));
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
-    
+
 
 });
 
